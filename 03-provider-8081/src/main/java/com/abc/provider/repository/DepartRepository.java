b5f0755a2ddef4961f34bd7e6161ff3c84e2343f
tom_glb <630glb@qq.com>
2020-02-13 11:21:31 +0800

    修改一行

--- 03-provider-8081/src/main/java/com/abc/provider/repository/DepartRepository.java
+++ 03-provider-8081/src/main/java/com/abc/provider/repository/DepartRepository.java
@@ -6,4 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 // 第一个泛型：当前Repository的操作对象类型
 // 第二个泛型：当前Repository的操作对象的id类型
 public interface DepartRepository extends JpaRepository<Depart, Integer> {
+
+    //加一行
 }
